72c6625570857b8927050069fea3d63976a323af
miguelalbox <49124664+miguelalbox@users.noreply.github.com>
2022-07-11 13:45:15 +0200

    [features] Add figure 1st form created #7

--- src/Controller/FigureController.php
+++ src/Controller/FigureController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Figures;
+use App\Form\FigureType;
+use App\Repository\FiguresRepository;
+use App\Repository\GroupsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class FigureController extends AbstractController
 {
@@ -16,7 +23,7 @@ class FigureController extends AbstractController
         ]);
     }
 
-    #[Route('/figure/{id}', name: 'single_figure')]
+    #[Route('/figures/figure/{id}', name: 'single_figure')]
     public function single(): Response
     {
         return $this->render('figure/single-figure.html.twig', [
@@ -24,21 +31,57 @@ class FigureController extends AbstractController
         ]);
     }
 
-    #[Route('/ajouter-figure', name: 'add_figure')]
-    public function add(): Response
+    #[Route('/figures/ajouter', name: 'add_figure')]
+    public function add(Request $request, EntityManagerInterface $manager, FiguresRepository $figureRepo, GroupsRepository $groupsRepo): Response
     {
+        $figure = new Figures;
+        $groups = $groupsRepo->findAll();
+        $groupsFigure = [];
+
+        foreach ($groups as $group){
+            $groupsFigure[$group->getFigureGroup()] = $group->getFigureGroup();
+        }
+        $form = $this->createForm(FigureType::class, $figure);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $title = $figure->getTitle();
+            $slugger = new AsciiSlugger();
+            $slug = strtolower($slugger->slug($title, '-'));
+            $i = 0;
+
+            do {
+                $existSlug = $figureRepo->findOneBy([
+                    'slug' => $slug
+                ]);
+                if ($existSlug != null) {
+                    $slug = strtolower($slugger->slug($title, '-') .'-'. $i);
+                    $i++;
+                }
+            } while ($existSlug != null);
+            $figure->setSlug($slug);
+
+            $figure->setCreatedAt(new \DateTimeImmutable());
+
+            $manager->persist($figure);
+            $manager->flush();
+            $this->addFlash("success", "La figure a bien été ajouté" );
+
+            return $this->redirectToRoute('all_figure');
+        }
         return $this->render('figure/add-edit-figure.html.twig', [
-            
+            'form' => $form->createView(),
         ]);
     }
-    #[Route('/editer-figure/{slug}', name: 'edit_figure')]
+    #[Route('/figures/editer/{slug}', name: 'edit_figure')]
     public function edit(): Response
     {
         return $this->render('figure/add-edit-figure.html.twig', [
             
         ]);
     }
-    #[Route('/suprimer-figure', name: 'delete_figure')]
+    #[Route('/figures/suprimer', name: 'delete_figure')]
     public function delete()//: Response
     {
        /* return $this->render('figure/add-edit-figure.html.twig', [
--- src/Form/FigureType.php
+++ src/Form/FigureType.php
@@ -3,8 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Figures;
+use App\Entity\Groups;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +14,6 @@ class FigureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        
         $builder
             ->add('title', TextType::class, [
                 'label' => 'Titre'
@@ -21,16 +21,9 @@ class FigureType extends AbstractType
             ->add('description', TextType::class, [
                 'label' => 'Description'
             ])
-            //TODO Dyn groups where id exemple in https://github.com/Eyefiz/eyefiz-digital/blob/main/src/Form/CarriereType.php
-            ->add('groups', ChoiceType::class, [
-                'label' => 'Groupe',
-                'choice' => [
-                    'Mc Twist' => 'Mc Twist',
-                    'Jib' => "Jib",
-                    'Grabs' => "Grabs",
-                    'Lipslide' => "Lipslide",
-                    'Air to Fakie' => "Air to Fakie"
-                ]
+            ->add('groups', EntityType::class, [
+                'class' => Groups::class,
+                'choice_label' => 'figure_group'
             ])
         ;
     }
