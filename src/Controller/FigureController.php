d41cae1b14fa719a58e8d72231ed28fe407c0471
miguelalbox <49124664+miguelalbox@users.noreply.github.com>
2022-08-28 17:56:36 +0200

    [Features] Fix bug entity groups #7  Ajout de btn edit del sur la figure

--- src/Controller/FigureController.php
+++ src/Controller/FigureController.php
@@ -43,7 +43,7 @@ class FigureController extends AbstractController
         $figuresAll = $paginator->paginate(
             $figures, // Requête contenant les données à paginer (ici nos articles)
             $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
-            2 // Nombre de résultats par page
+            8 // Nombre de résultats par page
         );
 
         $portrait = $mediaRepo->findBy([  'figure' => $figures]);
@@ -55,7 +55,7 @@ class FigureController extends AbstractController
             'figuresUser' => $figuresUser,
         ]);
     }
-    #[Route('/figures/mes-figures', name: 'all_figure_user')]
+    #[Route('/mes-figures', name: 'all_figure_user')]
     public function allFiguresUser(FiguresRepository $figuresRepo, MediaRepository $mediaRepo, PaginatorInterface $paginator, Request $request): Response
     {
         $user = $this->getUser();
@@ -64,7 +64,7 @@ class FigureController extends AbstractController
         $figuresAll = $paginator->paginate(
             $figures, // Requête contenant les données à paginer (ici nos articles)
             $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
-            2// Nombre de résultats par page
+            8// Nombre de résultats par page
         );
 
         $portrait = $mediaRepo->findBy([  'figure' => $figures]);
@@ -75,11 +75,25 @@ class FigureController extends AbstractController
         ]);
     }
 
-    #[Route('/figures/figure/{id}', name: 'single_figure')]
-    public function single(Figures $figures, MediaRepository $mediaRepo, Groups $groups): Response
+    #[Route('/figure/{id}', name: 'single_figure')]
+    public function single(Figures $figures, MediaRepository $mediaRepo,FiguresRepository $figuresRepo, GroupsRepository $groupsRepo): Response
     {
+        $groups = $groupsRepo->findAll();
+        $figureGroup = $figuresRepo->findOneBy(['groups' => $groups]);
+//dd($figureGroup->getGroups()->getFigureGroup());
+
         $figure = $figures;
-        $group = $groups;
+        
+
+
+        if ($this->getUser()){
+            $user = $this->getUser()->getId();
+            $figuresUser = $figuresRepo->findby(['user' => $user]);
+        }
+        else{
+            $user = null;
+            $figuresUser = null;
+        }
 
         $mediaExist = $mediaRepo->findBy(['figure' => $figure->getId()]);
         if ($mediaExist =! null) {
@@ -89,12 +103,15 @@ class FigureController extends AbstractController
         }
         //dd($group->getFigureGroup());
 
+
         return $this->render('figure/single-figure.html.twig', [
             'figure' => $figure,
             'mediasImg' => $ImgByFigure,
             'mediasVideo' => $VideoByFigure,
             'portrait' => $portrait,
-            'group' => $group->getFigureGroup(),
+            'group' => $figureGroup->getGroups()->getFigureGroup(),
+            'user' => $user,
+            'figuresUser' => $figuresUser,
         ]);
     }
 
@@ -103,15 +120,12 @@ class FigureController extends AbstractController
     {
 
 
-        $figure = new Figures;
-        //ajout d'utilisateur en session
-        $user = $this->getUser();
-        //dd($user);
-        if ($user == null) {
-            $this->addFlash('error', 'Vous devez d\'abord vous connecter');
-            return $this->redirectToRoute('all_figure');
-        }
 
+
+
+        $figure = new Figures;
+//ajout d'utilisateur en session
+        $user = $this->getUser();
         $groups = $groupsRepo->findAll();
         $groupsFigure = [];
 
@@ -254,7 +268,7 @@ class FigureController extends AbstractController
         ]);
     }
     #[Route('/figures/suprimer/{slug}', name: 'delete_figure')]
-    public function delete($slug, EntityManagerInterface $manager, FiguresRepository $figureRepo, Figures $figure, MediaRepository $mediaRepo, Media $media )//: Response
+    public function delete($slug, EntityManagerInterface $manager, FiguresRepository $figureRepo, Figures $figure, MediaRepository $mediaRepo)//: Response
     {
         $figureUser = $figure->getUser();
         //TODO securite, on ne peut pas acceder si on n'est pas l'utilisateur
@@ -262,12 +276,22 @@ class FigureController extends AbstractController
             $this->addFlash('error', 'La figure ne vous partien pas');
             return $this->redirectToRoute('all_figure');
         }
-        $figures = $figureRepo->findOneBy(['slug' => $slug]);
-        $mediasFigures = $mediaRepo->findBy(['figure' => $figure->getId()]);
 
+        $figures = $figureRepo->findOneBy(['slug' => $slug]);
+
+        $mediasFigures = $mediaRepo->findBy(['figure' => $figure->getId()]);
         //TODO suprimer image en cascade je recupere les images de la figure puis pour chaque une des entite je suprime le fichier avec un boucle for avec unlink
 
         //TODO il suprime une seul image pour le moment
+
+        /*$groupsImages = [];
+
+        foreach ($mediasFigures as $mediasFigure){
+            $groupsImages[$mediasFigure->getUrl()] = $mediasFigure->getUrl();
+        }
+        dd($groupsImages);
+
+
 
         /*foreach ($mediasFigures as $mediasFigure) {
             $url = $media->getUrl();
--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -50,7 +50,7 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-         return new RedirectResponse($this->urlGenerator->generate('homepage'));
+         return new RedirectResponse($this->urlGenerator->generate('all_figure'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
